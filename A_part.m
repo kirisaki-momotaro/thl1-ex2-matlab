b02be0009a47083264446dc2c4593fdec48afb25
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-04-30 12:23:10 +0300

    A2 start

--- A_part.m
+++ A_part.m
@@ -29,6 +29,29 @@ power_fftshift_SRRC = abs(fftshift_SRRC).^2;     % zero-centered power
 semilogy(freq,power_fftshift_SRRC)
 grid on;
 
+%A2
+N=100;
+b = (sign(randn(N, 1)) + 1)/2;
+X = bits_to_2PAM(b);
+
+X_delta = 1/Ts * upsample(X, over);
+figure(1)
+%X_delta_time = 0:N*over-1;
+X_delta_time = 0:Ts:N*Ts*over-Ts;
+plot(X_delta_time,X_delta);
+grid on;
+title('original signal')
+
+
+
+
+%create signal to be sent by sender
+signal = conv(X_delta,phi)*Ts;
+signal_t = [X_delta_time(1)+t(1):Ts:X_delta_time(end)+t(end)];
+figure(2)
+plot(signal_t,signal);
+grid on;
+title('modulated signal')
 
 
 
